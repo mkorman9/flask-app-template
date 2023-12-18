4ab7731291d3c15a02a734e5b0c7ed0919d4c6e2
Michal Korman <michal.korman@icloud.com>
2023-12-18 21:04:31 +0100

    lint

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,11 @@
 import atexit
 import os
 
-from gevent import get_hub
+import gevent
 
 from webapp.base import create_app_base
 
-get_hub().exception_stream = None
+gevent.get_hub().exception_stream = None
 
 app = create_app_base(__name__)
 
