69e36dabad74306336091ea3faffc58478d0212a
Michal Korman <michal.korman@icloud.com>
2023-12-20 09:45:47 +0100

    lint

--- webapp/logger.py
+++ webapp/logger.py
@@ -6,7 +6,7 @@ import gevent
 
 def configure_logger():
     logging.basicConfig(
-        level=os.environ.get("LOG_LEVEL", "INFO"),
+        level=os.environ.get('LOG_LEVEL', 'INFO'),
         format='[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s',
         force=True
     )
